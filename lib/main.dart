804c46ebff3e2fb1b942c5df7ad25a43fcfaf285
Daniel Alvarez <daniel.alvarez@nucodiga.com>
2024-11-19 11:21:42 -0600

    base login UI

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:debt_manager/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Debt Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+        ),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
